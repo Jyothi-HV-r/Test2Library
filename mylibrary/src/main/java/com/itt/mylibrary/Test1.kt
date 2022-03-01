25188670fcebc68e5c77fa685b7ff16ae4117bad
jyothi.hv <jyothi.hv@intimetec.com>
2022-03-01 13:47:52 +0530

    xxx

--- mylibrary/src/main/java/com/itt/mylibrary/Test1.kt
+++ mylibrary/src/main/java/com/itt/mylibrary/Test1.kt
@@ -1,6 +1,16 @@
 package com.itt.mylibrary
 
-class Test1 : Test() {
+import android.util.Log
+import com.google.gson.Gson
+
+open class Test1 : Test() {
     var x = Test1().name
+
+    fun getNumber(): Int {
+        var gson = Gson().toJson(Test1())
+        Log.d("test", gson)
+        return 12
+    }
+
 
 }
